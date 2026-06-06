26613f6f677baa8f40974d9c0f32aed987f1af5e
Sean <seanianking@gmail.com>
2026-06-05 19:34:31 -0600

    implemented rudimentary routing, installed tailwind and daisyui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,49 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Link, Outlet } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// 1. Create a Layout component to hold navigation links
+const Layout: React.FC = () => {
+  return (
+    <div>
+      <nav className="flex gap-2.5 p-2.5">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <hr />
+      {/* Outlet renders the matched child route element */}
+      <Outlet />
+    </div>
+  )
+}
+
+// 2. Define the route structure
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+])
+
+// 3. Render the app using RouterProvider
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  )
+}
